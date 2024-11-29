c24f12ed5e535aec48ec2f433005fdb92eb579a9
staudtMarius <marius.staudt@tu-dortmund.de>
2024-11-29 10:27:58 +0100

    Adapt to changes in API

--- src/main/java/edu/ie3/sample/EvModelImpl.java
+++ src/main/java/edu/ie3/sample/EvModelImpl.java
@@ -62,12 +62,12 @@ public class EvModelImpl implements EvModel {
   }
 
   @Override
-  public ComparableQuantity<Power> getSRatedAC() {
+  public ComparableQuantity<Power> getPRatedAC() {
     return sRatedAC;
   }
 
   @Override
-  public ComparableQuantity<Power> getSRatedDC() {
+  public ComparableQuantity<Power> getPRatedDC() {
     return sRatedDC;
   }
 
--- src/main/java/edu/ie3/sample/ExtLink.java
+++ src/main/java/edu/ie3/sample/ExtLink.java
@@ -7,13 +7,11 @@
 package edu.ie3.sample;
 
 import edu.ie3.simona.api.ExtLinkInterface;
-import edu.ie3.simona.api.data.ExtDataSimulation;
+import edu.ie3.simona.api.simulation.ExtSimAdapterData;
 import edu.ie3.simona.api.simulation.ExtSimulation;
-import java.util.ArrayList;
-import java.util.List;
 
 public class ExtLink implements ExtLinkInterface {
-  private final ExternalSampleSim sampleSim = new ExternalSampleSim();
+  private ExternalSampleSim sampleSim;
 
   @Override
   public ExtSimulation getExtSimulation() {
@@ -21,9 +19,7 @@ public class ExtLink implements ExtLinkInterface {
   }
 
   @Override
-  public List<ExtDataSimulation> getExtDataSimulations() {
-    ArrayList<ExtDataSimulation> list = new ArrayList<>();
-    list.add(sampleSim);
-    return list;
+  public void setup(ExtSimAdapterData data) {
+    sampleSim = new ExternalSampleSim("ExtSimSample");
   }
 }
